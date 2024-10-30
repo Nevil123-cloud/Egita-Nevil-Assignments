6d869a7b8490bb446e057fb5f19786b70c0bbf99
Egita Nevil <egitanevil@gmail.com>
2024-10-30 17:57:51 +0300

    first commit

---
+++ -/Nevil.py/Work.py
@@ -0,0 +1,34 @@
+#variables
+favourite-number=1000
+print(f"This is my favourite number:{favourite-number}") 
+
+#string
+greeting = "Good evening, PLP Academy"
+Print(greeting)
+
+#Boolean
+is _python _fun = True
+is_python_ boring= False
+print(f" is python fun?") {is python _fun}
+#print(f" is python_boring?) {is_python_ boring}")"
+
+#control Flow
+temperature = 30
+if temperature > 25:
+    print("its hot outside! wear shorts.")
+elif temperature >15:
+    Print("its warm. wear a light jacket.")
+else:
+    print("Ooops! its cold, bundle up")
+
+#modules
+#inbuilt module
+import math
+result= math.sqrt(60)
+print(f" the square root of 60 is, {result}")
+
+#customized
+import ekurole
+
+print(ekurole.ekurole("separation"))
+print(ekurole.ekurole("zuchu"))
---
+++ .bash_history
@@ -0,0 +1,7 @@
+git clone https://github.com/PLP-WebTechnologies/introduction-to-html-Nevil123-cloud.git
+git clone https://github.com/PLP-WebTechnologies/introduction-to-html-Nevil123-cloud.git
+cd Nevil123-cloud
+git branch -m master main
+git push -u origin main
+git clone https://github.com/PLP-Database-Design/week-1-assignment-Nevil123-cloud.git
+git clone https://github.com/PLP-Database-Design/week-2-assignment-Nevil123-cloud.git
